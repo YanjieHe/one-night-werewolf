aee096fded3b00cc49413834f93b94ca20198fa6
Yanjie He <heyanjie0@outlook.com>
2020-06-18 22:07:39 -0400

    add function 'generateEverything'

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,8 @@
 module Lib
     ( someFunc,
     allocateRoles,
-    writeGameInfo
+    writeGameInfo,
+    generateEverything
     ) where
 
 
@@ -411,6 +412,10 @@ writeGameInfo nPlayers nGames =
             (writeGlobalInfo (folderName i)
                 (originalPlayers, originalCardsOnTable) (players, cardsOnTable))) [1..nGames]
     where folderName = (\i -> "一夜狼人杀（" ++ (show nPlayers) ++ "）人局/游戏" ++ (show i))
+
+
+generateEverything :: IO [[()]]
+generateEverything = (mapM (\i -> (writeGameInfo i 50)) [6..10])
 
 
 someFunc :: IO ()
